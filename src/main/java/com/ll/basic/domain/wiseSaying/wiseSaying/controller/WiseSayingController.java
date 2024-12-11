bd90e5fd99f93349124bed5d148989b8c0e99559
kk0506014 <kk0506014@gmail.com>
2024-12-11 09:40:18 +0900

    wiseSaying update

---
+++ src/main/java/com/ll/basic/domain/wiseSaying/wiseSaying/controller/WiseSayingController.java
@@ -0,0 +1,40 @@
+package com.ll.basic.domain.wiseSaying.wiseSaying.controller;
+
+import com.ll.basic.domain.wiseSaying.wiseSaying.entity.WiseSaying;
+import com.ll.basic.domain.wiseSaying.wiseSaying.service.WiseSayingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
+public class WiseSayingController {
+    private final WiseSayingService wiseSayingService;
+
+    @GetMapping("/wiseSayings")
+    public List<WiseSaying> getItems() {
+        return wiseSayingService.findAll();
+    }
+
+    @GetMapping("/wiseSayings/write")
+    public WiseSaying write(
+            String content,
+            @RequestParam(defaultValue = "무명") String author
+    ) {
+        return wiseSayingService.write(content, author);
+    }
+
+    @GetMapping("/wiseSayings/{id}")
+    public WiseSaying getItem(
+            @PathVariable long id
+    ) {
+        Optional<WiseSaying> opWiseSaying = wiseSayingService.findById(id);
+
+        return opWiseSaying.get();
+    }
+}
---
+++ src/main/java/com/ll/basic/domain/wiseSaying/wiseSaying/service/WiseSayingService.java
@@ -0,0 +1,65 @@
+package com.ll.basic.domain.wiseSaying.wiseSaying.service;
+
+import com.ll.basic.domain.wiseSaying.wiseSaying.entity.WiseSaying;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class WiseSayingService {
+    private final List<WiseSaying> wiseSayings;
+    private long lastId;
+
+    public WiseSayingService() {
+        this.lastId = 0;
+        this.wiseSayings = new ArrayList<>() {{
+            add(
+                    WiseSaying
+                            .builder()
+                            .id(++lastId)
+                            .content("삶이 있는 한 희망은 있다.")
+                            .author("키케로")
+                            .build()
+            );
+
+            add(
+                    WiseSaying
+                            .builder()
+                            .id(++lastId)
+                            .content("삶이 있는 한 희망은 있다.")
+                            .author("키케로")
+                            .build()
+            );
+        }};
+    }
+
+    public List<WiseSaying> findAll() {
+        return wiseSayings
+                .stream()
+                .sorted(Comparator.comparing(WiseSaying::getId).reversed())
+                .toList();
+    }
+
+    public WiseSaying write(String content, String author) {
+        WiseSaying wiseSaying = WiseSaying
+                .builder()
+                .id(++lastId)
+                .content(content)
+                .author(author)
+                .build();
+
+        wiseSayings.add(wiseSaying);
+
+        return wiseSaying;
+    }
+
+    public Optional<WiseSaying> findById(long id) {
+        return wiseSayings
+                .stream()
+                .filter(wiseSaying -> wiseSaying.getId() == id)
+                .findFirst();
+    }
+}
